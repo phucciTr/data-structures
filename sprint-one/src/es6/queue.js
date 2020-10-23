70908e8990c940f8c86a9b4b72ff5e79496ad852
phucciTr <miabam26@gmail.com>
2020-10-22 18:26:20 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,33 @@
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.currentSize = 0;
+    this.frontIndex = 0;
+    this.rearIndex = 0;
+  }
+
+  enqueue(value) {
+    let enqueueIndex = this.rearIndex;
+    this[enqueueIndex] = value;
+    this.currentSize++;
+    this.rearIndex++;
+  }
+
+  dequeue() {
+    if (this.size() > 0) {
+      let dequeueIndex = this.frontIndex;
+      let dequeuedVal = this[dequeueIndex];
+      this.currentSize--;
+      this.frontIndex++;
+      delete this[dequeueIndex];
+      return dequeuedVal;
+    }
+  }
+
+  size() {
+    return this.currentSize;
   }
 
 }
