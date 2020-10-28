854856db3ec76a34f7bf443aeba57122dd42714d
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2020-10-28 14:27:16 -0500

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -17,7 +17,7 @@ treeMethods.addChild = function(value) {
 treeMethods.contains = function(target) {
 
   if (this.value === target) { return true; }
-
+  stepCount++;
   for (let i = 0; i < this.children.length; i++) {
     let childTree = this.children[i];
     if (childTree.contains(target)) { return true; }
@@ -30,9 +30,24 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * addChild is O(1) because we are pushing the new node to the end of the array. The time complexity of push is O(1).
+ * contains is O(n) where n is the number of nodes (subtrees) in the root tree because we only have to visit each of the
+ * subtrees once in order to find if target is present within that subtrees. Worst case scenario is having to visit each
+ * subtree because target is not present.
+ *
  */
 
 var sampleTree = Tree();
 
 // Test Case
+
+
+var stepCount = 0;
+
 sampleTree.addChild(5);
+sampleTree.addChild(6);
+sampleTree.children[0].addChild(7);
+sampleTree.children[1].addChild(8);
+sampleTree.contains(8);
+console.log(stepCount);
