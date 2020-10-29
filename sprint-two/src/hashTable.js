fbcb9a968f291f21e98c6161c0cf05f6bab27ae4
phucciTr <miabam26@gmail.com>
2020-10-29 12:47:08 -0700

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,90 +1,58 @@
-var LimitedArray = function(limit) {
-  var storage = [];
-
-  var limitedArray = {};
-  limitedArray.get = function(index) {
-    checkLimit(index);
-    return storage[index];
-  };
-  limitedArray.set = function(index, value) {
-    checkLimit(index);
-    storage[index] = value;
-  };
-  limitedArray.each = function(callback) {
-    for (var i = 0; i < storage.length; i++) {
-      callback(storage[i], i, storage);
-    }
-  };
-
-  var checkLimit = function(index) {
-    if (typeof index !== 'number') {
-      throw new Error('setter requires a numeric index for its first argument');
-    }
-    if (limit <= index) {
-      throw new Error('Error trying to access an over-the-limit index');
-    }
-  };
-
-  return limitedArray;
-};
-
-// This is a "hashing function". You don't need to worry about it, just use it
-// to turn any string into an integer that is well-distributed between the
-// numbers 0 and `max`
-var getIndexBelowMaxForKey = function(str, max) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32bit integer
-    hash = Math.abs(hash);
-  }
-  return hash % max;
-};
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
-
-
-
-
-
-
-
-
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  let currentBucket = this._storage.get(index);
+  if (currentBucket === undefined) { currentBucket = []; }
+
+  // checking to see if k already exists
+  // if k already exists, we replace previous value with new v
+  for (let i = 0; i < currentBucket.length; i++) {
+    let currentTuple = currentBucket[i];
+    let key = currentTuple[0];
+    if (key === k) {
+      currentTuple[1] = v;
+      return;
+    }
+  }
+
+  let tuple = [k, v];
+  currentBucket.push(tuple);
+  this._storage.set(index, currentBucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+
+  let currentBucket = this._storage.get(index);
+
+  for (let i = 0; i < currentBucket.length; i++) {
+    let currentTuple = currentBucket[i];
+    let key = currentTuple[0];
+    if (key === k) { return currentTuple[1]; }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  console.log(storage);
 
-  // this._storage = this._storage.storage.splice(index, 1);
+  var valAtK = this.retrieve(k);
 
-  return this._storage;
+  if (valAtK) { this.insert(k, undefined); }
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert    : Worst case scenario, our hashing function puts all the elements in the same bucket, therefore we must traverse all the elements and our time complexity is O(n)
+ *             However, if our hashing function creates a uniformly distrubuted range of hash codes..
+ *             the time complexity will model O(1), because there is a trivial number of tuples inside of each bucket relative to the size of our input
+ * retrieve  : The time complexity is O(1) following the logic above
+ * remove    : The time complexity is O(1) because it relies on the retrive() & insert() methods
  */
-var hashSet = new HashTable();
-hashSet.insert('Steven', 'Tyler');
-hashSet.remove('Steven');
