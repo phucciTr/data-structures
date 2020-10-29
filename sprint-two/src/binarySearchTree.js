6b3d946463f15c563fed4859fa11c1f95d52d8af
phucciTr <miabam26@gmail.com>
2020-10-29 13:28:45 -0700

    Pushing for the end of the day

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,45 @@
 var BinarySearchTree = function(value) {
+  let newBSTree = {};
+  newBSTree.left = null;
+  newBSTree.right = null;
+  newBSTree.value = value;
+  _.extend(newBSTree, BSTreeMethods);
+  return newBSTree;
+};
+
+var BSTreeMethods = {};
+
+
+
+BSTreeMethods.insert = function(v) {
+  if (this.value === null) {
+    this.value = v;
+    return this.value;
+  }
+  if (this.left === null && v < this.value) {
+    let newLeftChild = BinarySearchTree(v);
+    this.left = newLeftChild;
+  }
+  if (this.right === null && v > this.value) {
+    let newRightChild = BinarySearchTree(v);
+    this.right = newRightChild;
+  }
+
+  if (v < this.value) {
+    this.left.value = this.left.insert(v);
+  }
+  if (v > this.value) {
+    this.right.value = this.right.insert(v);
+  }
+
+};
+
+BSTreeMethods.contains = function(v) {
+
+};
+
+BSTreeMethods.depthFirstLog = function(callback) {
+
 };
 
 
