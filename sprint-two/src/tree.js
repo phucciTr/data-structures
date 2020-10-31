d786027be43fac69ab2457b9bfd05ecbdeb99139
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2020-10-31 12:55:00 -0500

    added method .traverse with callback function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -25,14 +25,32 @@ treeMethods.contains = function(target) {
 
 };
 
+treeMethods.traverse = function(cb) {
+  cb(this.value);
+  for (let i = 0; i < this.children.length; i++) {
+    let childTree = this.children[i];
+    childTree.traverse(cb);
+  }
+};
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  *
- * addChild is O(1) because we are pushing the new node to the end of the array. The time complexity of push is O(1).
- * contains is O(n) where n is the number of nodes (subtrees) in the root tree because we only have to visit each of the
- * subtrees once in order to find if target is present within that subtrees. Worst case scenario is having to visit each
- * subtree because target is not present.
+ * addChild is O(1) because we are pushing the new node to the end of the array. The time complexity of push is
+ * O(1) contains is O(n) where n is the number of nodes (subtrees) in the root tree because we only have to
+ * visit each of the subtrees once in order to find if target is present within that subtrees. Worst case
+ * scenario is having to visit each subtree because target is not present.
  *
+ */
+
+
+
+/**
+ *          null
+ *         /    \
+ *        5      6
+ *       /        \
+ *      7          8
  */
